f1a0997e59b69957f285f84567d683973edca9da
yogeshkannanV <152466959+yogeshkannanV@users.noreply.github.com>
2024-06-13 15:37:31 +0530

    Add files via upload

---
+++ src/components/bodyComponents/customer/Customers.js
@@ -0,0 +1,190 @@
+import productList from "../inventory/productList";
+
+const customers = [{
+    id:1,firstName: "ADMIN",
+    lastName: "NEW",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:2,firstName: "ADMIN",
+    lastName: "NEW",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+}
+,{
+    id:3,firstName: "ADMIN",
+    lastName: "NEW",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:4,firstName: "ADMIN",
+    lastName: "NEW",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:5,firstName: "ADMIN",
+    lastName: "NEW",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+}
+,{
+    id:6,firstName: "ADMIN",
+    lastName: "NEW",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:7,firstName: "ADMIN",
+    lastName: "NEW",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:8,firstName: "ADMIN",
+    lastName: "NEW",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+}
+,{
+    id:9,firstName: "ADMIN",
+    lastName: "NEW",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:10,firstName: "ADMIN",
+    lastName: "NEW",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:11,firstName: "ADMIN",
+    lastName: "NEW",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:12,firstName: "ADMIN",
+    lastName: "NEW",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+}
+,{
+    id:13,firstName: "ADMIN",
+    lastName: "NEW",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:14,firstName: "ADMIN",
+    lastName: "NEW",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:15,firstName: "ADMIN",
+    lastName: "NEW",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+}
+,{
+    id:16,firstName: "ADMIN",
+    lastName: "NEW",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:17,firstName: "ADMIN",
+    lastName: "NEW",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:18,firstName: "ADMIN",
+    lastName: "NEW",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+}
+,{
+    id:19,firstName: "ADMIN",
+    lastName: "NEW",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+},{
+    id:20,firstName: "VIJAY",
+    lastName: "ANAND",
+    position: "Software Engineer",
+    mobile: "+212 6 51 88 61 51",
+    orders:[{
+        id: 1,
+        products: [{quantity:5,product:productList[0]}, {quantity:5,product:productList[1]},{quantity:5,product: productList[2]}],}
+    ]
+}];
+export default customers;
